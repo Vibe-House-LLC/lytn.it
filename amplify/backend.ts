1beffd9223c3fc4833904f81a560954748214048
d3layd <dennis.layden@gmail.com>
2025-07-15 10:16:33 -0500

    remove import function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,7 +4,6 @@ import { data } from './data/resource';
 import { vainId } from './functions/vainId/resource';
 import { emailReportedLink } from './functions/emailReportedLink/resource';
 import { userManagement } from './functions/userManagement/resource';
-import { linkImporter } from './functions/linkImporter/resource';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -15,12 +14,10 @@ export const backend = defineBackend({
   vainId,
   emailReportedLink,
   userManagement,
-  linkImporter
 });
 
 const emailReportedLinkLambda = backend.emailReportedLink.resources.lambda
 const userManagementLambda = backend.userManagement.resources.lambda
-const linkImporterLambda = backend.linkImporter.resources.lambda
 
 const emailStatement = new PolicyStatement({
   actions: ['ses:SendEmail'],
@@ -48,26 +45,3 @@ const cognitoStatement = new PolicyStatement({
 
 emailReportedLinkLambda.addToRolePolicy(emailStatement)
 userManagementLambda.addToRolePolicy(cognitoStatement)
-
-// Grant linkImporter permission to access DynamoDB tables
-backend.linkImporter.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    actions: [
-      'dynamodb:PutItem',
-      'dynamodb:BatchWriteItem',
-      'dynamodb:Query',
-      'dynamodb:Scan',
-      'dynamodb:GetItem'
-    ],
-    resources: [
-      backend.data.resources.tables["shortenedUrl"].tableArn,
-      `${backend.data.resources.tables["shortenedUrl"].tableArn}/index/*`
-    ]
-  })
-)
-
-// Add table name as environment variable
-backend.linkImporter.addEnvironment(
-  'AMPLIFY_DATA_SHORTENEDURL_TABLE_NAME',
-  backend.data.resources.tables["shortenedUrl"].tableName
-)
--- amplify/functions/linkImporter/resource.ts
+++
@@ -1,7 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-export const linkImporter = defineFunction({
-  name: 'linkImporter',
-  entry: './handler.ts',
-  timeoutSeconds: 60 * 5, // 5 minutes for large imports
-});
