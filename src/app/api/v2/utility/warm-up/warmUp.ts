602652248841358b862fddbd619fc063949e1425
AustinWoodard <93208415+AustinWoodard@users.noreply.github.com>
2025-07-28 14:18:00 -0400

    Add warmup function nextjs api

---
+++ src/app/api/v2/utility/warm-up/warmUp.ts
@@ -0,0 +1,13 @@
+import { cookiesClient } from "@/utilities/amplify-utils";
+import getClientAuthType from "@/utilities/clientAuthType";
+
+
+const client = cookiesClient;
+
+const warmUp = async () => {
+    const authMode = await getClientAuthType()
+    const vainId = await client.queries.vainId({ warmup: true}, { authMode });
+    return vainId;
+}
+
+export default warmUp;
